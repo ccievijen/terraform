a10f7cc631ab77842d3b19c2293cafa49b51b895
VIJEN <vijen.negi@gmail.com>
2019-12-11 15:38:09 -0500

    centos 7 terraform

--- Terraform/ec2-centos.tf
+++ Terraform/ec2-centos.tf
@@ -5,7 +5,7 @@ data "aws_ami" "centos" {
 
   filter {
     name   = "name"
-    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+    values = ["CentOS Linux new 7 x86_64 HVM EBS *"]
   }
 
   filter {
